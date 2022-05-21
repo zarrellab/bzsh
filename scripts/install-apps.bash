4d2677ff0cb176e03a3fde7e1e630275c02aced0
Brandon Zarrella <bzarrella@splunk.com>
2022-05-21 15:32:55 -0600

    harden install script

--- scripts/install-apps.bash
+++ scripts/install-apps.bash
@@ -10,167 +10,214 @@ else
   brew_prefix=/usr/local
 fi
 
-echo 'Installing homebrew...'
-NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-eval "$($brew_prefix/bin/brew shellenv)"
+if (! command -v brew &>/dev/null); then
+  echo 'Installing homebrew...'
+  NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  eval "$($brew_prefix/bin/brew shellenv)"
+fi
 
-brew update
+if (! command -v brew &>/dev/null); then
+  echo 'Error: homebrew is not found!'
+  echo 'Make sure homebrew is installed and in path!'
+  exit 1
+else
+  brew update
 
-brew tap chrokh/tap
-brew tap homebrew/cask
-brew tap homebrew/cask-drivers
-brew tap homebrew/cask-fonts
-brew tap homebrew/cask-versions
+  brew tap chrokh/tap
+  brew tap homebrew/cask
+  brew tap homebrew/cask-drivers
+  brew tap homebrew/cask-fonts
+  brew tap homebrew/cask-versions
 
-brew install \
-  1password \
-  1password-cli \
-  aha \
-  asdf \
-  autojump \
-  awscli \
-  bat \
-  brotli \
-  cmake \
-  coreutils \
-  curl \
-  direnv \
-  discord \
-  docker-slim \
-  epic-games \
-  figma \
-  firefox \
-  firefox-developer-edition \
-  font-sauce-code-pro-nerd-font \
-  fzf \
-  gawk \
-  gifox \
-  git \
-  gnupg \
-  gnutls \
-  google-chrome \
-  helm \
-  htop \
-  htop \
-  imagemagick \
-  imageoptim-cli \
-  iterm2 \
-  iterm2 \
-  jq \
-  kubectx \
-  mas \
-  microsoft-edge \
-  neovim \
-  pigz \
-  pinentry-mac \
-  pipenv \
-  postman \
-  python \
-  quicklook-csv \
-  quicklook-json \
-  ripgrep \
-  rustup-init \
-  sequel-pro \
-  shellcheck \
-  slack \
-  steam \
-  subversion \
-  svgo \
-  the-unarchiver \
-  tldr \
-  transmission-cli \
-  tree \
-  visual-studio-code \
-  watch \
-  watchman \
-  wget \
-  zinit \
-  zsh \
-  zsh-completions
+  brew install 1password
+  brew install 1password-cli
+  brew install aha
+  brew install asdf
+  brew install autojump
+  brew install awscli
+  brew install bat
+  brew install brotli
+  brew install cmake
+  brew install coreutils
+  brew install curl
+  brew install direnv
+  brew install discord
+  brew install docker-slim
+  brew install epic-games
+  brew install figma
+  brew install firefox
+  brew install firefox-developer-edition
+  brew install font-sauce-code-pro-nerd-font
+  brew install fzf
+  brew install gawk
+  brew install gifox
+  brew install git
+  brew install gnupg
+  brew install gnutls
+  brew install google-chrome
+  brew install helm
+  brew install htop
+  brew install htop
+  brew install imagemagick
+  brew install imageoptim-cli
+  brew install iterm2
+  brew install iterm2
+  brew install jq
+  brew install kubectx
+  brew install mas
+  brew install microsoft-edge
+  brew install neovim
+  brew install pigz
+  brew install pinentry-mac
+  brew install pipenv
+  brew install postman
+  brew install python
+  brew install quicklook-csv
+  brew install quicklook-json
+  brew install ripgrep
+  brew install rustup-init
+  brew install sequel-pro
+  brew install shellcheck
+  brew install slack
+  brew install steam
+  brew install subversion
+  brew install svgo
+  brew install the-unarchiver
+  brew install tldr
+  brew install transmission-cli
+  brew install tree
+  brew install visual-studio-code
+  brew install watch
+  brew install watchman
+  brew install wget
+  brew install zinit
+  brew install zsh
+  brew install zsh-completions
 
-brew install --cask \
-  adobe-creative-cloud \
-  cinebench \
-  docker \
-  monitorcontrol \
-  mouse-fix \
-  obsidian \
-  rectangle \
-  transmission \
-  vlc \
-  zoom
+  brew install --cask adobe-creative-cloud
+  brew install --cask cinebench
+  brew install --cask docker
+  brew install --cask monitorcontrol
+  brew install --cask mouse-fix
+  brew install --cask obsidian
+  brew install --cask rectangle
+  brew install --cask transmission
+  brew install --cask vlc
+  brew install --cask zoom
 
-echo 'Installing App Store apps...'
-mas lucky amphetamine
-mas lucky keynote
-mas lucky nordvpn
-mas lucky numbers
-mas lucky pages
+  # install fzf completions
+  "$(brew --prefix)"/opt/fzf/install
 
-# install fzf completions
-"$(brew --prefix)"/opt/fzf/install
+  echo 'Checking for issues...'
+  brew doctor
+fi
 
-echo 'Checking for issues...'
-brew doctor
+if (! command -v mas &>/dev/null); then
+  echo 'Error: cannot install apps from App Store'
+  echo 'Make sure mas is installed from brew!'
+  exit 1
+else
+  echo 'Installing App Store apps...'
+  mas lucky amphetamine
+  mas lucky keynote
+  mas lucky nordvpn
+  mas lucky numbers
+  mas lucky pages
+fi
 
 echo 'Configuring related utils...'
 
-echo 'Starting docker...'
-open /Applications/Docker.app
+# Get docker started for later steps because it can take a while
+if (! command -v docker &>/dev/null); then
+  echo 'Error: docker not found!'
+  echo 'Make sure docker is installed from brew!'
+  exit 1
+else
+  if (! docker info &>/dev/null); then
+    echo "Launching Docker..."
+    open /Applications/Docker.app
+  fi
+fi
 
-echo 'Installing node...'
-# Install node version manager
-asdf plugin add nodejs
-# Install the default version of nodejs
-asdf install nodejs latest
-asdf global nodejs latest
-# Enable yarn
-corepack enable
+if (! command -v asdf &>/dev/null); then
+  echo 'Error: asdf not found!'
+  echo 'Make sure asdf is installed from brew!'
+  exit 1
+else
+  echo 'Installing node...'
+  # Install node version manager
+  asdf plugin add nodejs
+  # Install the default version of nodejs
+  asdf install nodejs latest
+  asdf global nodejs latest
+  # Enable yarn
+  corepack enable
 
-echo 'Installing java...'
-# Install java version manager
-asdf plugin-add java
-# Install the default version of java
-asdf install java adoptopenjdk-jre-18.0.1+10
-asdf global java adoptopenjdk-jre-18.0.1+10
+  echo 'Installing java...'
+  # Install java version manager
+  asdf plugin-add java
+  # Install the default version of java
+  asdf install java adoptopenjdk-jre-18.0.1+10
+  asdf global java adoptopenjdk-jre-18.0.1+10
 
-echo 'Installing maven...'
-# Install maven version manager
-asdf plugin-add maven
-# Install the default version of maven
-asdf install maven latest
-asdf global maven latest
+  echo 'Installing maven...'
+  # Install maven version manager
+  asdf plugin-add maven
+  # Install the default version of maven
+  asdf install maven latest
+  asdf global maven latest
 
-echo 'Installing python...'
-# Install python version manager
-asdf plugin-add python
-# Install the default version of python
-asdf install python latest
-asdf global python latest
-# Upgrade pip
-pip3 install --upgrade pip
+  echo 'Installing python...'
+  # Install python version manager
+  asdf plugin-add python
+  # Install the default version of python
+  asdf install python latest
+  asdf global python latest
+  # Upgrade pip
+  pip3 install --upgrade pip
 
-echo 'Installing rust...'
-# Install rust version manager
-asdf plugin-add rust
-# Install the default version of rust
-asdf install rust latest
-asdf global rust latest
+  echo 'Installing rust...'
+  # Install rust version manager
+  asdf plugin-add rust
+  # Install the default version of rust
+  asdf install rust latest
+  asdf global rust latest
 
-echo 'Installing scala...'
-# Install scala version manager
-asdf plugin-add scala
-# Install the default version of scala
-asdf install scala latest
-asdf global scala latest
+  echo 'Installing scala...'
+  # Install scala version manager
+  asdf plugin-add scala
+  # Install the default version of scala
+  asdf install scala latest
+  asdf global scala latest
+fi
 
-echo 'Configuring gpg...'
-echo "pinentry-program $(brew --prefix)/bin/pinentry-mac" >>~/.gnupg/gpg-agent.conf
+if (! command -v gpg &>/dev/null); then
+  echo 'Error: gpg not found!'
+  echo 'Make sure gpg is installed from brew!'
+  exit 1
+else
+  echo 'Configuring gpg...'
+  echo "pinentry-program $(brew --prefix)/bin/pinentry-mac" >>~/.gnupg/gpg-agent.conf
+fi
 
 echo 'Installing iterm utils...'
 # iterm shell integration
 curl -L https://iterm2.com/shell_integration/install_shell_integration_and_utilities.sh | zsh
 
-echo 'Ensure docker is running before continuing!'
-read -r -p '?Press any key to continue...'
-docker login
+if (! command -v docker &>/dev/null); then
+  echo 'Error: docker not found!'
+  echo 'Make sure docker is installed from brew!'
+  exit 1
+else
+  if (! docker info &>/dev/null); then
+    echo "Launching Docker..."
+    open /Applications/Docker.app
+    sleep 10
+    while (! docker info &>/dev/null); do
+      echo "Waiting for Docker to launch..."
+      sleep 10
+    done
+  fi
+
+  docker login
+fi
